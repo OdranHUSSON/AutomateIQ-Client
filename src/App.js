40883edce1d000a6b6d4c91ef007b58eeaca4777
Odran HUSSON <odran@webforger.fr>
2023-04-10 18:52:07 +0200

    :hammer: work in progress websocket implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import io from 'socket.io-client';
+import axios from 'axios';
+
+const apiUrl = 'http://localhost:3000';
+const socket = io(apiUrl);
 
 function App() {
+  const [details, setDetails] = useState({
+    context: '',
+    actors: '',
+    initiative: '',
+  });
+
+  const [jobId, setJobId] = useState(null);
+  const [progress, setProgress] = useState(0);
+  const [done, setDone] = useState(false);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setDetails({ ...details, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setProgress(0);
+    setDone(false);
+    const jobId = await generatePRD(details, 1);
+    setJobId(jobId);
+  };
+
+  socket.on('connect', () => {
+    console.log('Connected to server');
+    socket.emit('message', 'Hello from client');
+  });
+
+  socket.on('update', (data) => {
+    console.log(`Server says job ${data.jobId} has progressed to : ${data.progress}`);
+    if (data.jobId === jobId) {
+      setProgress(data.progress);
+      if (data.progress === 100) {
+        setDone(true);
+      }
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Disconnected from server');
+  });
+
+  socket.on('connect_error', (err) => {
+    console.log(`connect_error due to ${err.message}`);
+  });
+
+  const generatePRD = async (details, temperature) => {
+    try {
+      const response = await axios.post(apiUrl + '/api/generatePRD', {
+        details: details,
+        temperature: temperature,
+      });
+      return response.data.jobId;
+    } catch (error) {
+      console.log(error);
+      throw new Error(error.response.data.error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Context:
+          <input type="text" name="context" value={details.context} onChange={handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Actors:
+          <input type="text" name="actors" value={details.actors} onChange={handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Initiative:
+          <input type="text" name="initiative" value={details.initiative} onChange={handleInputChange} />
+        </label>
+        <br />
+        <button type="submit">Generate PRD</button>
+      </form>
+      {jobId && (
+        <div>
+          <p>Generating with jobId: {jobId}</p>
+          {progress > 0 && <p>Progress: {progress}%</p>}
+          {done && <p>Done!</p>}
+        </div>
+      )}
     </div>
   );
 }
